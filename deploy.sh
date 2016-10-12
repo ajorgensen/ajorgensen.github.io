7ed6238e7f418e302bca4fced5c4954540d5ddaf
Andrew Jorgensen <andrew@andrewjorgensen.com>
2016-10-11 22:01:55 -0400

    rebuilding site Tue Oct 11 22:01:55 EDT 2016

--- deploy.sh
+++ deploy.sh
@@ -17,4 +17,4 @@ git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
-git subtree push --prefix=public https://github.com/ajorgensen/ajorgensen.github.io.git gh-pages
+git subtree push --prefix=public https://github.com/ajorgensen/blog.git gh-pages
