7e1cbe18c44fb794b4a37f33523128339aa6bf3c
Andrew Jorgensen <andrew@andrewjorgensen.com>
2017-06-01 16:25:16 -0400

    rebuilding site Thu Jun  1 16:25:16 EDT 2017

--- deploy.sh
+++ deploy.sh
@@ -13,20 +13,27 @@ fi
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
+echo -e "\033[0;32mBuilding site...\033[0m"
 hugo
 
-# Add changes to git.
-git add -A
+git diff-index --quiet HEAD --
+if [ $? -ne 0 ]; then
+  # Commit changes.
+  msg="rebuilding site `date`"
+  if [ $# -eq 1 ];then
+    msg="$1"
+  fi
 
-# Commit changes.
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
+  echo -e "\033[0;32mCommitting changes with message $msg...\033[0m"
+  # Add changes to git.
+  git add -A
+
+  git commit -m "$msg"
+
+  # Push source and build repos.
+  git push origin master
 fi
-git commit -m "$msg"
-
-# Push source and build repos.
-git push origin master
 
 # Deploy to firebase
+echo -e "\033[0;32mDeploying to firebase hosting...\033[0m"
 firebase deploy
