548de11e76694abd178770698eccd15529febc45
Andrew Jorgensen <andrew@andrewjorgensen.com>
2022-12-21 10:47:18 -0500

    rewrite using tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,49 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["layouts/**/*.html", "content/**/*.{html, md}"],
+
+  theme: {
+    extend: {
+      colors: {
+        transparent: "transparent",
+        accent: "#FF572D",
+        "dark-background": "#202630",
+        "light-background": "#F5F5F5",
+      },
+
+      typography: (theme) => ({
+        default: {
+          css: {
+            pre: {
+              color: theme("colors.grey.1000"),
+              backgroundColor: theme("colors.grey.100"),
+            },
+            "pre code::before": {
+              "padding-left": "unset",
+            },
+            "pre code::after": {
+              "padding-right": "unset",
+            },
+            code: {
+              backgroundColor: theme("colors.grey.100"),
+              color: "#DD1144",
+              fontWeight: "400",
+              "border-radius": "0.25rem",
+            },
+            "code::before": {
+              content: '""',
+              "padding-left": "0.25rem",
+            },
+            "code::after": {
+              content: '""',
+              "padding-right": "0.25rem",
+            },
+          },
+        },
+      }),
+    },
+  },
+
+  plugins: [require("@tailwindcss/typography")],
+};
